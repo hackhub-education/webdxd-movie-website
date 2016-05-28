60ed72d78c6fb855cfd1f9f7fb17e0e396c5f721
Yan Hong <onlyhongyan@gmail.com>
2016-05-28 12:05:05 -0700

    add sold out to button buy

--- js/ticket.js
+++ js/ticket.js
@@ -1,4 +1,9 @@
 $('.btn-buy').click(function(event){
   var newTicketLeft = $(event.target).parent().prev().text() - 1;
-  $(event.target).parent().prev().text(newTicketLeft);
+  if (newTicketLeft >= 0) {
+    $(event.target).parent().prev().text(newTicketLeft);
+    if (newTicketLeft === 0) {
+      $(event.target).text("Sold Out").prop('disabled', 'disabled').addClass('btn-disabled').removeClass('primary-btn btn-buy');
+    }
+  }
 });
