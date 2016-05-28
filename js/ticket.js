2e47d1cf04234087b99a2713c1b1e0e9a213977a
Yan Hong <onlyhongyan@gmail.com>
2016-05-28 14:33:49 -0700

    add buy ticket modal

--- js/ticket.js
+++ js/ticket.js
@@ -1,9 +1,36 @@
+var buttonClicked;
+
 $('.btn-buy').click(function(event){
-  var newTicketLeft = $(event.target).parent().prev().text() - 1;
+  buttonClicked = event.target;
+  var movieName = $(event.target).parent().parent().find('.movie-name').text();
+  $('#buy-ticket-modal .modal-header').html('<h5>Buy ' + movieName + ' Ticket</h5>');
+});
+
+
+$('#buy-ticket-btn').click(function(event) {
+  var user = {
+    first_name: $('#first_name').val(),
+    last_name: $('#last_name').val(),
+    phone: $('#phone').val()
+  };
+  console.log(user);
+
+  var newTicketLeft = $(buttonClicked).parent().prev().text() - 1;
   if (newTicketLeft >= 0) {
-    $(event.target).parent().prev().text(newTicketLeft);
+    $(buttonClicked).parent().prev().text(newTicketLeft);
     if (newTicketLeft === 0) {
-      $(event.target).text("Sold Out").prop('disabled', 'disabled').addClass('btn-disabled').removeClass('primary-btn btn-buy');
+      $(buttonClicked).text("Sold Out").prop('disabled', 'disabled').addClass('btn-disabled').removeClass('primary-btn btn-buy');
     }
   }
+
+  $('#buy-ticket-modal').modal('hide');
+
 });
+
+
+
+
+
+
+
+
